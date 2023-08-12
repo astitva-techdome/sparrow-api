d19c8bb9dd6690d27dbadaff4e77da1fb180bae5
Nayan Lakhwani <nayanlakhwani@Nayans-MacBook-Pro.local>
2023-08-12 13:52:02 +0530

    mana

---
+++ src/modules/common/config/configuration.ts
@@ -0,0 +1,15 @@
+import { Env } from "./env.validation";
+
+export default () => ({
+  app: {
+    port: parseInt(process.env.PORT) || 3000,
+    env: process.env.APP_ENV || Env.DEV,
+    url: process.env.APP_URL,
+    webtokenSecretKey: process.env.WEBTOKEN_SECRET_KEY,
+    webtokenExpirationTime:
+      parseInt(process.env.WEBTOKEN_EXPIRATION_TIME) || 1800,
+  },
+  db: {
+    url: process.env.DB_URL,
+  },
+});
