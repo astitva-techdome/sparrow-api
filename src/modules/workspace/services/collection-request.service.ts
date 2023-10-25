0e81b9f99fcf9834466f53daf6ed340f549fae29
prathamesh thakare <prathamesh06pt@gmail.com>
2023-10-25 13:32:18 +0530

    feat: add dates and name infos to requests in collection [SP-236]

--- src/modules/workspace/services/collection-request.service.ts
+++ src/modules/workspace/services/collection-request.service.ts
@@ -135,6 +135,7 @@ export class CollectionRequestService {
     collectionId: string,
     request: CollectionRequestDto,
     noOfRequests: number,
+    userName: string,
   ): Promise<UpdateResult<Collection>> {
     try {
       const uuid = uuidv4();
@@ -144,9 +145,14 @@ export class CollectionRequestService {
         type: request.items.type,
         description: request.items.description,
       };
-
+      const requestInfo = {
+        createdBy: userName,
+        updatedBy: userName,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
       if (request.items.type === ItemTypeEnum.REQUEST) {
-        requestObj.request = request.items.request;
+        requestObj.request = { ...request.items.request, ...requestInfo };
         return await this.collectionReposistory.addRequest(
           collectionId,
           requestObj,
@@ -159,7 +165,7 @@ export class CollectionRequestService {
             name: request.items.items.name,
             type: request.items.items.type,
             description: request.items.items.description,
-            request: request.items.items.request,
+            request: { ...request.items.items.request, ...requestInfo },
           },
         ];
 
